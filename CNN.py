248aae05433c87486a012479f23368d104367634
Wu Changrong <wcr@live.cn>
2020-01-01 16:12:22 +0800

    small fix

--- CNN.py
+++ CNN.py
@@ -3,23 +3,18 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import Embedding
 from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D
-import sklearn.metrics as sm
+from keras.utils import np_utils
 
 import LoadDataset
-
-def prec_score_CNN(y_true, y_pred):
-    return sm.precision_score(y_true, y_pred, average='macro')
-
-def f1_score_CNN(y_true, y_pred):
-    return sm.f1_score(y_true, y_pred, average='macro')
 
 if __name__ == "__main__":
     dataset = LoadDataset.Dataset("./CTU-13-Dataset")
     dataset.loadData()
     train_dataset, train_labels, test_dataset, test_labels = dataset.getEntireDataset()
+    train_labels = np_utils.to_categorical(train_labels, num_classes=3, dtype='int')
 
     model = Sequential()
-    model.add(Conv1D(64, 3, activation='relu', input_shape=(None, 14)))
+    model.add(Conv1D(64, 3, activation='relu', input_shape=(14, 1)))
     model.add(Conv1D(64, 3, activation='relu'))
     model.add(MaxPooling1D(3))
     model.add(Conv1D(128, 3, activation='relu'))
@@ -30,7 +25,7 @@ if __name__ == "__main__":
 
     model.compile(loss=keras.losses.categorical_hinge,
                 optimizer='rmsprop',
-                metrics=[prec_score_CNN, f1_score_CNN])
+                metrics=['mae'])
 
     model.fit(train_dataset, train_labels, batch_size=512, epochs=10)
     res = model.predict(test_dataset, batch_size=512)
