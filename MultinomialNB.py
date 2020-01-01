0aa4d491decc15429403c26b72971125f1e7383e
Wu Changrong <wcr@live.cn>
2020-01-01 11:06:01 +0800

    fix and add prec score and add fs output

--- MultinomialNB.py
+++ MultinomialNB.py
@@ -19,7 +19,22 @@ def score_MulNB(predict_labels, test_labels):
     return sm.balanced_accuracy_score(test_labels, predict_labels)
 
 def f1_score_MulNB(predict_labels, test_labels):
-    return sm.f1_score(test_labels, predict_labels)
+    return sm.f1_score(test_labels, predict_labels, average='macro')
+
+def prec_score_MulNB(predict_labels, test_labels):
+    t_predict_labels = predict_labels
+    t_test_labels = test_labels
+    for i in range(0, len(predict_labels)):
+        if predict_labels[i] == 2:
+            t_predict_labels[i] = 1
+        else:
+            t_predict_labels[i] = 0
+
+        if test_labels[i] == 2:
+            t_test_labels[i] = 1
+        else:
+            t_test_labels[i] = 0
+    return sm.precision_score(t_test_labels, t_predict_labels)
 
 def experiment(train_dataset, test_dataset, train_labels, test_labels=None):
     MulNB_model = train_MulNB(train_dataset, train_labels)
@@ -27,6 +42,7 @@ def experiment(train_dataset, test_dataset, train_labels, test_labels=None):
     if test_labels != None:
         # print("balanced accuracy: {0}".format(score_MulNB(res, test_labels)))
         print("F1 score: {0}".format(f1_score_MulNB(res, test_labels)))
+        print("Precision score: {0}".format(prec_score_MulNB(res, test_labels)))
 
     return res
 
@@ -36,3 +52,6 @@ if __name__ == "__main__":
     train_dataset, train_labels, test_dataset, test_labels = dataset.getEntireDataset()
 
     res = experiment(train_dataset, test_dataset, train_labels, test_labels)
+    with open("MulNB_predict.txt", 'w') as file:
+        for label in res:
+            file.write(str(label)+"\n")
