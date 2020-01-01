ac23de10e92188857507e02a8571d33a2daba8fe
Wu Changrong <wcr@live.cn>
2020-01-01 20:18:01 +0800

    promote

--- LSTM.py
+++ LSTM.py
@@ -4,7 +4,7 @@ import numpy
 from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import Embedding
-from keras.layers import Conv1D, GlobalAveragePooling1D, MaxPooling1D
+from keras.layers import LSTM
 from keras.utils import np_utils
 
 import LoadDataset
@@ -14,8 +14,8 @@ if __name__ == "__main__":
     dataset = LoadDataset.Dataset("./CTU-13-Dataset")
     dataset.loadData()
     train_dataset, train_labels, test_dataset, test_labels = dataset.getEntireDataset()
-    train_dataset = numpy.array(train_dataset).reshape((len(train_dataset), 14, 1))
-    test_dataset = numpy.array(test_dataset).reshape((len(test_dataset), 14, 1))
+    train_dataset = numpy.array(train_dataset).reshape((len(train_dataset), 1, 14))
+    test_dataset = numpy.array(test_dataset).reshape((len(test_dataset), 1, 14))
     train_labels = np_utils.to_categorical(train_labels, num_classes=class_num, dtype='int')
 
     # get training model
@@ -23,14 +23,10 @@ if __name__ == "__main__":
     if len(sys.argv) == 1:
         # create a new model
         model = Sequential()
-        model.add(Conv1D(64, 2, activation='relu', input_shape=(14, 1)))
-        model.add(Conv1D(64, 2, activation='relu'))
-        model.add(MaxPooling1D(2))
-        model.add(Conv1D(128, 2, activation='relu'))
-        model.add(Conv1D(128, 2, activation='relu'))
-        model.add(GlobalAveragePooling1D())
+        model.add(Embedding(14, output_dim=10))
+        model.add(LSTM(128))
         model.add(Dropout(0.5))
-        model.add(Dense(class_num, activation='sigmoid'))
+        model.add(Dense(1, activation='sigmoid'))
 
         sgd = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
         model.compile(loss=keras.losses.categorical_hinge,
@@ -46,7 +42,7 @@ if __name__ == "__main__":
         epochs = int(sys.argv[2])
 
     model.fit(train_dataset, train_labels, batch_size=512, epochs=epochs)
-    model.save("CNN.model")
+    model.save("LSTM.model")
     res = model.predict(test_dataset, batch_size=512)
     with open("CNN_predict.result", 'w') as file:
         for label in res:
