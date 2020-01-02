94cb97d34010ac78ba64756435ccabd5e69d5653
Wu Changrong <wcr@live.cn>
2020-01-02 12:41:58 +0800

    fix LSTM

--- LSTM.py
+++ LSTM.py
@@ -20,8 +20,8 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         # create a new model
         model = Sequential()
-        model.add(LSTM(32, input_shape=(1, 14)))
-        model.add(LSTM(32))
+        model.add(LSTM(32, return_sequences=True, input_shape=(1, 14)))
+        model.add(LSTM(32, return_sequences=True))
         model.add(LSTM(32))
         model.add(Dense(3, activation='softmax'))
         sgd = keras.optimizers.SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
