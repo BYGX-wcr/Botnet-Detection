0d6aa9db6ea53c6ac7a3a2bf5cd2d45f3946f003
Wu Changrong <wcr@live.cn>
2020-01-03 13:05:38 +0800

    fix CNN

--- CNN.py
+++ CNN.py
@@ -20,9 +20,13 @@ if __name__ == "__main__":
     if len(sys.argv) < 3:
         # create a new model
         model = Sequential()
-        model.add(Conv1D(32, 2, activation='relu', input_shape=(14, 1)))
-        model.add(Conv1D(32, 2, activation='relu'))
-        model.add(MaxPooling1D())
+        model.add(Conv1D(32, 3, activation='relu', input_shape=(14, 1)))
+        model.add(Conv1D(32, 3, activation='relu'))
+        model.add(MaxPooling1D(2))
+        model.add(Dropout(0.5))
+        model.add(Conv1D(64, 3, activation='relu'))
+        model.add(Conv1D(64, 3, activation='relu'))
+        model.add(GlobalAveragePooling1D())
         model.add(Dropout(0.5))
         model.add(Dense(3, activation='softmax'))
 
